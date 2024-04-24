0021edbec4d995be3051332f4c1369bfd8bc4b8f
Husna Ramadan <husna.ramadan@gmail.com>
2024-04-24 09:21:44 +0700

    Clearing

---
+++ src/routes/produk/+page.svelte
@@ -0,0 +1,17 @@
+<div class="mx-auto bg-primary-400 md:max-w-3xl">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officia sed dicta, ullam dolorem
+	excepturi eaque illo vitae dignissimos eveniet iste iure omnis enim. Nesciunt magnam sint iure
+	voluptate cumque? Magni rerum doloribus cupiditate facilis, quam, dolorum, odit reiciendis
+	mollitia maiores quas hic similique a alias beatae suscipit nobis quod animi nihil velit ullam
+	aut? Rem nihil consectetur vero quidem. Reiciendis pariatur dicta modi a dolor odit quas
+	perferendis rerum assumenda in, aut unde mollitia quidem voluptates facilis tempore quae
+	exercitationem expedita minima! Temporibus sint nisi velit odio esse cumque. Iste molestiae
+	doloribus sequi ducimus voluptatum mollitia repellendus necessitatibus eum accusantium provident,
+	ullam voluptate neque. Libero distinctio modi beatae id alias esse, odit obcaecati nulla repellat
+	veniam culpa hic necessitatibus! Voluptas dolore consequatur, cumque aliquam obcaecati nesciunt
+	nisi temporibus iusto fugiat perspiciatis? Eum commodi ratione, earum impedit facere illo suscipit
+	assumenda provident, aliquam, nulla harum blanditiis vitae asperiores ullam repudiandae? Ratione,
+	adipisci non! Dolore optio hic deserunt molestias voluptatem, eos temporibus nobis facilis
+	reiciendis ex repellat quia non tenetur sit quod praesentium, dolorum sapiente! Accusamus modi
+	reprehenderit repellat magni ea.
+</div>
